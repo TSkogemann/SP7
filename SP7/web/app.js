39175bbbe5de1d81f70b45c636636d52f7d78e37
Thomas Skogemann <skogemon@hotmail.com>
2016-08-10 00:27:02 +0200

    final

--- SP7/web/app.js
+++ SP7/web/app.js
@@ -1,23 +1,39 @@
 angular.module('examApp', [])
-        .factory('dataService', function () {
-            var studentsInfo = {};
-            studentsInfo.allCourses = [
-                {courseId: 1000, courseName: "Basic Programming"},
-                {courseId: 1001, courseName: "Advanced Programming"},
-                {courseId: 1003, courseName: "DataBase Intro"}];
-            studentsInfo.students = [];
-            studentsInfo.students.push({studentId: 100, name: "Peter Hansen", grades: [{grade: "10"}, {grade: "12"}, {}]});
-            studentsInfo.students.push({studentId: 101, name: "Jan Olsen", grades: [{grade: "7"}, {grade: "10"}, {}]});
-            studentsInfo.students.push({studentId: 102, name: "Gitte Poulsen", grades: [{grade: "7"}, {grade: "7"}, {}]});
-            studentsInfo.students.push({studentId: 103, name: "John McDonald", grades: [{grade: "10"}, {}, {grade: "7"}]});
+        .factory('dataService', function ($http) {
 
-            return {
+
+            var studentsInfo = {};
+            var data = {
                 studentsInfo: studentsInfo
             };
+            var dataSource = 'hardcode';
+            if (dataSource === 'hardcode') {
+                studentsInfo.allCourses = [
+                    {courseId: 1000, courseName: "Basic Programming"},
+                    {courseId: 1001, courseName: "Advanced Programming"},
+                    {courseId: 1003, courseName: "DataBase Intro"}];
+                studentsInfo.students = [];
+                studentsInfo.students.push({studentId: 100, name: "Peter Hansen", grades: [{grade: "10"}, {grade: "12"}, {}]});
+                studentsInfo.students.push({studentId: 101, name: "Jan Olsen", grades: [{grade: "7"}, {grade: "10"}, {}]});
+                studentsInfo.students.push({studentId: 102, name: "Gitte Poulsen", grades: [{grade: "7"}, {grade: "7"}, {}]});
+                studentsInfo.students.push({studentId: 103, name: "John McDonald", grades: [{grade: "10"}, {}, {grade: "7"}]});
+            } else
+            {
+                $http({
+                    'url': 'server/rest/studensInfo',
+                    method: 'GET'
+                }).then(function (response) {
+                    studentsInfo = response.data;
+                });
+            }
+            return {
+                data: data
+            };
+
         })
         .controller('ExamController', function (dataService) {
-                    var model = this;
-                    model.studentsInfo = dataService.studentsInfo;
+            var model = this;
+            model.studentsInfo = dataService.data.studentsInfo;
 
         })
         .filter('averageGrade', function () {
